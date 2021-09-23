5ffd729b90222f9daa6af2b74beca11bf2a2b6a0
MohamedAbdisamed <Abdisamed@hotmail.dk>
2021-09-23 17:41:59 +0200

    Update kalender.js

--- UCL Community/JS/kalender.js
+++ UCL Community/JS/kalender.js
@@ -1,6 +1,13 @@
-const date= new Date();
+const date = new Date();
 
-const month= [
+const monthDays = document.querySelctor('days')
+
+const lastDay = new Date(date.getFullYear(),
+date.getMonth()+ 0, 1)
+
+console.log(lastDay);
+
+const month = [
 "Januar"
 "Februar"
 "Marts"
@@ -16,7 +23,13 @@ const month= [
 
 ]
 document.querySelctor(".date h1").innerHTML
-=month [date.geMonth()];
+= month [date.geMonth()];
 
 document.querySelctor(".date p").innerHTML
-=date.toDateString();
+= date.toDateString();
+
+let days = "";
+for(let i = 1; i <= 31; i ++){
+  days += '<div>{i}</div>';
+  mothDays.innerHTML = days;
+}
