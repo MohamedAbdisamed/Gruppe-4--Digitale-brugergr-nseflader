fb6460b8d9e10744c00c49e2b4c6d7812e5b4df2
MohamedAbdisamed <Abdisamed@hotmail.dk>
2021-09-23 19:58:21 +0200

    Update kalender.js
    oprettelse af hele kalender via js

--- UCL Community/JS/kalender.js
+++ UCL Community/JS/kalender.js
@@ -1,11 +1,13 @@
 const date = new Date();
 
+date.setMonth(5);
+
 const monthDays = document.querySelctor('days')
 
 const lastDay = new Date(date.getFullYear(),
-date.getMonth()+ 0, 1)
+date.getMonth()+ 1, 0);
 
-console.log(lastDay);
+console.log(lastDay).getDate();
 
 const month = [
 "Januar"
@@ -29,7 +31,7 @@ document.querySelctor(".date p").innerHTML
 = date.toDateString();
 
 let days = "";
-for(let i = 1; i <= 31; i ++){
+for(let i = 1; i <= lastDay; i ++){
   days += '<div>{i}</div>';
   mothDays.innerHTML = days;
 }
