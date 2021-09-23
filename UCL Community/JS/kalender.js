e2ed83bdeec7d30030a06da7667985b63fc133ee
MohamedAbdisamed <Abdisamed@hotmail.dk>
2021-09-23 17:14:55 +0200

    igang med js til kalender

---
+++ UCL Community/JS/kalender.js
@@ -0,0 +1,19 @@
+const date= new Date();
+
+const month= [
+"Januar"
+"Februar"
+"Marts"
+"April"
+"Maj"
+"Juni"
+"July"
+"August"
+"September"
+"Oktober"
+"November"
+"December"
+
+]
+document.querySelctor(".date h1").innerHTML
+=month [5]
